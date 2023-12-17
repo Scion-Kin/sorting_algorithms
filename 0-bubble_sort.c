e1e27aaface94cc822f3f79d1e21380f34badf88
Scion-Kin <dinturner17@gmail.com>
2023-12-17 12:01:28 +0200

    Bubble sort fixed

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,27 +10,34 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j, k;
-	int temp;
+	int temp, swapped;
 
 	if (!array || size == 0)
 	{
 		return;
 	}
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		for (j = 0; j < size; j++)
+		swapped = 0;
+
+		for (j = 0; j < size - 1 - i; j++)
 		{
 			if (array[j + 1] < array[j])
 			{
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-				printf("%d", array[0]);
-				for (k = 1; k < size; k++)
-					printf(", %d", array[k]);
-				printf("\n");
+				swapped = 1;
 			}
+		}
+
+		if (swapped == 1)
+		{
+			printf("%d", array[0]);
+			for (k = 1; k < size; k++)
+				printf(", %d", array[k]);
+			printf("\n");
 		}
 	}
 }
