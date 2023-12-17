604b59998acb3836781d5d61e0927252fc147531
Scion-Kin <dinturner17@gmail.com>
2023-12-17 12:11:31 +0200

    Bubble sort fixed

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,34 +9,35 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j, k;
-	int temp, swapped;
+	size_t i, j;
+	int temp;
+	int swapped; /* Flag to check if any swaps occurred in a pass */
 
 	if (!array || size == 0)
-	{
 		return;
-	}
 
 	for (i = 0; i < size - 1; i++)
 	{
-		swapped = 0;
+		swapped = 0; /* Reset the flag at the beginning of each pass */
 
 		for (j = 0; j < size - 1 - i; j++)
 		{
-			if (array[j + 1] < array[j])
+			if (array[j] > array[j + 1])
 			{
+				/* Swap elements */
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-				swapped = 1;
+				swapped = 1; /* Set the flag since a swap occurred */
 			}
 		}
 
-		if (swapped == 1)
+		/* Print the array after each pass if any swaps occurred */
+		if (swapped)
 		{
 			printf("%d", array[0]);
-			for (k = 1; k < size; k++)
-				printf(", %d", array[k]);
+			for (j = 1; j < size; j++)
+				printf(", %d", array[j]);
 			printf("\n");
 		}
 	}
